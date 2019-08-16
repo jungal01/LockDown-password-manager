9ebbc9efc1764a869852e317315d3b4eb530d9b1
Allen <jungal01@luther.edu>
2019-08-16 17:46:06 -0500

    start experiments with RSA and add package requirements

---
+++ RSAexperiment.py
@@ -0,0 +1,34 @@
+# This is a demo file for using RSA for it's main purposes in this project.
+# Once this, and other files like it, are no longer needed, a gist will be made
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+# the passphrase and message to be passed around
+# messages seem to require binary format to work with the API
+phrase = 'totallyRandomPassphrase'
+message = open('apartments.txt', 'rb').readline()
+
+# create and export encrypted keys
+# according to pycryptodome docs, this is the only way to encrypt a key, though
+# protecion can be any supported algo
+key = RSA.generate(4096)
+encrypted_key = key.exportKey(passphrase=phrase, pkcs=8, protection='scryptAndAES256-CBC')
+
+with open('privateRSAkey.key', 'wb') as f:
+    f.write(encrypted_key)
+    
+with open('publicRSAkey.key', 'wb') as f:
+    f.write(key.publickey().exportKey())
+
+
+# encrypt a message
+public_key = RSA.importKey(open('publicRSAkey.key').read())
+encrypt_cipher = PKCS1_OAEP.new(public_key)
+ciphertext = encrypt_cipher.encrypt(message)
+
+# decrypt a message
+# because the private key is encrypted, the passphrase needs to be included to allow decryption
+private_key = RSA.importKey(open('privateRSAkey.key').read(), phrase)
+decrypt_cipher = PKCS1_OAEP.new(private_key)
+plaintext = decrypt_cipher.decrypt(ciphertext)
